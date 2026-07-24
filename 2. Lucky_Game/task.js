3c5705a38868b027d508eea1e2871cd5fd83027a
Muhammad Abdullah <onlinestoresuperclack@gmail.com>
2026-07-24 15:09:34 +0500

    Update task.js

--- 2. Lucky_Game/task.js
+++ 2. Lucky_Game/task.js
@@ -1,21 +1,31 @@
-let randomNumber = Math.floor(Math.random() * 20 + 1) 
+let randomNumber = Math.floor(Math.random() * 20 + 1)
 // console.log(randomNumber);
 
 let playerChances = 5;
 let playerAttempts = 0;
 let playerWon = false;
+
 for (let i = 0; i < playerChances; i++) {
-    let numberGuess = +prompt("Attempt" + playerAttempts + "of 5 Guess a number between 1 and 20")
-    if(numberGuess == randomNumber) {
-        alert("Congratulations! You Won.")
-        playerAttempts++
+    let numberGuess = +prompt(
+        "Attempt " + (i + 1) + " of " + playerChances +
+        "\nGuess a number between 1 and 20"
+    );
+
+    playerAttempts++;
+
+    if (numberGuess == randomNumber) {
+        alert("Congratulations! You Won.");
         playerWon = true;
         break;
     } else {
-        alert("Wrong guess, try again")
-        playerAttempts++
+        alert("Wrong guess, try again.");
     }
 }
-if(playerWon == false) {
-    alert("Better Luck Next Time. \nCorrect Number: " + randomNumber + "\nAttempts Used: " + playerAttempts)
+
+if (!playerWon) {
+    alert(
+        "Better Luck Next Time." +
+        "\nCorrect Number: " + randomNumber +
+        "\nAttempts Used: " + playerAttempts
+    );
 }
